107e3a19bfa7176aedb7ace7c6be8316b9fae612
jang <dkjang@naver.com>
2020-08-10 21:37:04 +0900

    메인화면 리스트뷰추가 학생정보 데이터클래스 추가

---
+++ app/src/main/java/com/kakao/listview_20200810/datas/Student.kt
@@ -0,0 +1,13 @@
+package com.kakao.listview_20200810.datas
+
+class Student (
+    val name: String,
+    val address: String,
+    val isMale: Boolean,
+    val birthYear: Int
+) {
+//  학생이 갖는 하위정보(이름/거주지/성별/생년)
+//  멤버변수로 만들자 => 생성자를 멤버변수까지 동시에 세팅
+//  장동국/은평구/남/1900
+
+}
