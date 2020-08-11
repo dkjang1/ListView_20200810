fe3fe18898f6c2c6dfbba56e6f5332860885aa43
jang <dkjang@naver.com>
2020-08-11 19:56:07 +0900

    어댑터를 객체화 리스트뷰연결

--- app/src/main/java/com/kakao/listview_20200810/MainActivity.kt
+++ app/src/main/java/com/kakao/listview_20200810/MainActivity.kt
@@ -2,13 +2,20 @@ package com.kakao.listview_20200810
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kakao.listview_20200810.adapters.StudentAdapter
 import com.kakao.listview_20200810.datas.Student
+import kotlinx.android.synthetic.main.activity_main.*
 import java.security.KeyStore
 
 class MainActivity : AppCompatActivity() {
 
 //    학생명부가 담길 목록변수
     val mStudentList = ArrayList<Student>()
+
+//    Adapter는 멤버변수 초기화할수없다
+//    초기화면 나중으로 미루자
+
+    lateinit var mStudentAdapter: StudentAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +30,21 @@ class MainActivity : AppCompatActivity() {
         mStudentList.add(Student("이용찬","서울시 노원구", true, 1976))
         mStudentList.add(Student("장동국","서울시 은평구", true, 1974))
         mStudentList.add(Student("최인선","서울시 영등포구", false, 1992))
+
+        mStudentList.add(Student("조경진","서울시 은평구", true, 1988))
+        mStudentList.add(Student("강윤정","경기도 안양시", false, 1991))
+        mStudentList.add(Student("구주영","서울시 용산구", true, 1968))
+        mStudentList.add(Student("윤은영","경기도 수원시", false, 1992))
+        mStudentList.add(Student("이선영","경기도 고양시", false, 1985))
+        mStudentList.add(Student("이용찬","서울시 노원구", true, 1976))
+        mStudentList.add(Student("장동국","서울시 은평구", true, 1974))
+        mStudentList.add(Student("최인선","서울시 영등포구", false, 1992))
+
+//        Adapter변수 onCreate 내부에서 초기화함
+        mStudentAdapter = StudentAdapter(this, R.layout.student_list_item, mStudentList)
+
+//        xml에 있는 리스트뷰의 어댑터 지정
+        studentListView.adapter = mStudentAdapter
 
     }
 }
